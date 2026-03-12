d1dace38a0eab827d0d02b604dc0b37d2db0148b
papolahimanshu5402 <140336770+papolahimanshu5402@users.noreply.github.com>
2026-03-13 03:37:26 +0530

    fix express wildcard route

--- server/index.js
+++ server/index.js
@@ -46,7 +46,7 @@ const __dirname = path.resolve();
 if (process.env.NODE_ENV === "production") {
     const clientBuildPath = path.join(__dirname, "../client/build");
     app.use(express.static(clientBuildPath));
-    app.get("*", (req, res) => {
+    app.get("/*", (req, res) => {
         res.sendFile(path.resolve(clientBuildPath, "index.html"));
     });
 }
